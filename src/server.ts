6eff9f212609ef44ca0abf732561bb422bb57ecf
Ortizzz021 <dortizortiz021@gmail.com>
2026-06-01 15:48:32 -0500

    refactor: update CORS middleware configuration for improved security

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,15 @@ const app = express();
 
 // Security Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://amargo-studios.vercel.app',
+    ],
+    credentials: true,
+  })
+);
 
 // Body parser
 app.use(express.json());
